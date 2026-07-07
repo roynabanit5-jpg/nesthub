882556f46b1397c88ab649b3287af37d85c8ffb6
roynabanit5-jpg <roynabanit5@gmail.com>
2026-07-07 13:35:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+// NestHub Cart
 
+let cartCount = 0;
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+
+button.addEventListener("click", function(){
+
+cartCount++;
+
+alert("Item added to cart ✅");
+
+console.log("Cart Items:", cartCount);
+
+});
+
+});
