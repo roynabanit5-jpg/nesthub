16b19a49a430dc0d8d5a3cc81c49673f41363682
roynabanit5-jpg <roynabanit5@gmail.com>
2026-07-07 14:10:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -34,3 +34,29 @@ window.location.href="index.html";
 });
 
 }
+// NestHub Search
+
+const searchInput = document.querySelector(".search-box input");
+const products = document.querySelectorAll(".product-card");
+
+if(searchInput){
+
+searchInput.addEventListener("keyup",function(){
+
+const value = this.value.toLowerCase();
+
+products.forEach(product=>{
+
+const name = product.querySelector("h3").textContent.toLowerCase();
+
+if(name.includes(value)){
+product.style.display="block";
+}else{
+product.style.display="none";
+}
+
+});
+
+});
+
+}
